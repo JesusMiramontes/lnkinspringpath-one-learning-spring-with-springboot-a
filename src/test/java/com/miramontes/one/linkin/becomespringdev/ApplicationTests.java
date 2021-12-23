4d8412cd1fb80bee967e840269c9a8c956ff7c47
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-23 11:50:40 -0600

    init commit

---
+++ src/test/java/com/miramontes/one/linkin/becomespringdev/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miramontes.one.linkin.becomespringdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
